d7338cbbe9026e6f0c5498971e3c7fab81bd6f47
slasjh <127065284+slasjh@users.noreply.github.com>
2024-12-31 03:26:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -131,10 +131,10 @@ def updateChannelUrlsM3U(channels, template_channels):
                 if category in channels:
                     for channel_name in channel_list:
                         if channel_name in channels[category]:
-                            sorted_urls = sorted(channels[category][channel_name], key=lambda url: not is_ipv6(url) if config.ip_version_priority == "ipv6" else is_ipv6(url))
+                            sorted_urls = sorted(channels[category][channel_name], key=lambda url: is_ipv6(url) if config.ip_version_priority == "ipv6" else is_ipv6(url))
                             filtered_urls = []
                             for url in sorted_urls:
-                                if url and url not in written_urls and not any(blacklist in url for blacklist in config.url_blacklist):
+                                if url and  is_ipv6(url) and url not in written_urls and not any(blacklist in url for blacklist in config.url_blacklist):
                                     filtered_urls.append(url)
                                     written_urls.add(url)
 
