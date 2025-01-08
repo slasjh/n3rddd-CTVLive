5a916486bdab1679b828c9e44eb8b02a082b80eb
slasjh <127065284+slasjh@users.noreply.github.com>
2025-01-08 03:45:43 +0000

    Update blacklist4.py

--- blacklist1/blacklist4.py
+++ blacklist1/blacklist4.py
@@ -170,7 +170,7 @@ def measure_speed(url):
     headers = {
 
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
     }
 
     try:
