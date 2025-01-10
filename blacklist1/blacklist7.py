154bb8505c91a274085101aa95cfb81096851cf8
slasjh <127065284+slasjh@users.noreply.github.com>
2025-01-10 03:27:34 +0000

    Update blacklist7.py

--- blacklist1/blacklist7.py
+++ blacklist1/blacklist7.py
@@ -76,6 +76,8 @@ def measure_speed(url):
         #'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Connection': 'keep-alive',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Accept-Encoding': 'gzip, deflate',
     }
 
     ts_url = None
