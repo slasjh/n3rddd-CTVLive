76146900233587eca5c175f44ea7bb1471b4190a
slasjh <127065284+slasjh@users.noreply.github.com>
2025-01-13 06:37:32 +0000

    Update sort-2.py

--- sort/sort-2.py
+++ sort/sort-2.py
@@ -210,7 +210,7 @@ if __name__ == "__main__":
      # 写入央视频道卫视频道到input_file2
      # Define the categories to extract (as a set)
     channels_to_extract = {'央视频道', '卫视频道'}
-    extract_channels(out_file1, out_file2 channels_to_extract)
+    extract_channels(out_file1, out_file2, channels_to_extract)
     print("央视频道卫视频道写入tvlist-yw.txt完成。")
 
 
