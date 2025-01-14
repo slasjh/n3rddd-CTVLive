a0bb191d0c7123fde4ab16d8e07d708f228250f6
slasjh <127065284+slasjh@users.noreply.github.com>
2025-01-14 00:50:27 +0000

    Update blacklist9.py

--- blacklist1/blacklist9.py
+++ blacklist1/blacklist9.py
@@ -378,7 +378,7 @@ if __name__ == "__main__":
         #"https://gitlab.com/p2v5/wangtv/-/raw/main/lunbo.txt",
         #'https://gitlab.com/p2v5/wangtv/-/raw/main/wang-tvlive.txt'
         #'https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/refs/heads/main/live.txt',
-        'https://raw.githubusercontent.com/slasjh/n3rddd-CTVLive/refs/heads/ipv4/lite.txt'
+        'https://raw.githubusercontent.com/slasjh/n3rddd-CTVLive/refs/heads/ipv4/live.txt'
     ]
     for url in urls:
         print(f"处理URL: {url}")
